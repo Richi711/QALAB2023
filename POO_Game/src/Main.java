b7ac89481ce8aa6afaf7da5f1c2479efd204d2a1
Ricardo Javier Paterno <ricardojavier.paterno@emeal.nttdata.com>
2023-11-26 16:29:03 -0300

    se agrega coemntario al POO_Game

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        //editado por Ricardo Javier Paterno
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
